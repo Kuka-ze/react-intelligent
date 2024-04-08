b334458044bd5571f240a6bd40be2ae74075708d
帅泽 <vuejs1024@163.com>
2024-04-08 18:21:38 +0800

    usesstate

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import { useState } from "react";
+import './index.css'
 
 function App() {
+  const [form, setForm] = useState({ name: 'jack' })
+
+  const handleChange = () => {
+    setForm({
+      name: 'tom' 
+    })
+  }
   return (
     <div className="App">
-      this is App
+      {form.name}
+      <div onClick={handleChange}> 1111</div>
     </div>
   );
 }
