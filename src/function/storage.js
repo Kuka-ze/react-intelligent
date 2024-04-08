cf909f4852bbcf10eb2661bd01849c33a3fafb2b
帅泽 <vuejs1024@163.com>
2024-04-08 18:32:09 +0800

    增加掉用方法

--- src/function/storage.js
+++ src/function/storage.js
@@ -31,3 +31,9 @@ export default class Storage {
         localStorage.clear();
     }
 }
+
+// import Storage from 'xx/storage.js'
+// const storage1 = new Storage(24*60*60*1000); // 设置全局默认过期时间为24小时
+// storage1.set('name', 'nan'); // 使用全局默认过期时间
+// storage1.set('age', 18, 60*1000); // 设置独立的过期时间为1分钟
+
